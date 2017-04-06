7bfd8f6d6194fd506dece1c55338aad72c6ec69f
Eric Sowell <eric.sowell@gmail.com>
2017-04-06 09:59:02 -0500

    inline block and something else

--- day-18-gmail/script.js
+++ day-18-gmail/script.js
@@ -6,6 +6,7 @@ var theList = document.querySelector('ol');
 theList.addEventListener('click', function(evt) {
 
   var previouslySelected = document.querySelector('.selected');
+  console.log(previouslySelected);
   if (previouslySelected !== null) {
     previouslySelected.classList.remove('selected');
   }
