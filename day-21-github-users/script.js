0fa8178e1719f62fd9627c6d73a770cc8f6f1519
Eric Sowell <eric.sowell@gmail.com>
2017-04-11 14:09:12 -0500

    github users input change

--- day-21-github-users/script.js
+++ day-21-github-users/script.js
@@ -1,29 +1,44 @@
 
 
 
-var promise = $.ajax({
-  url: 'https://api.github.com/search/users?q=steak'
-});
 
 var listOfUsers = document.querySelector('#list-of-users');
+var searchQuery = document.querySelector('#search-query');
 
-promise.done(function(data) {
 
-  for (var i = 0; i < data.items.length; i++) {
-    var li = document.createElement('li');
+searchQuery.addEventListener('keyup', function(evt) {
 
-    var img = document.createElement('img');
-    img.src = data.items[i].avatar_url;
-    li.appendChild(img);
-
-    var a = document.createElement('a');
-    a.href = data.items[i].html_url;
-    a.target = '_blank';
-    a.textContent = data.items[i].login;
-
-    li.appendChild(a);
-
-    listOfUsers.appendChild(li);
+  if (evt.keyCode !== 13) {
+    return;
   }
+
+  listOfUsers.innerHTML = '';
+
+  var promise = $.ajax({
+    url: 'https://api.github.com/search/users?q=' + searchQuery.value
+  });
+
+
+  promise.done(function(data) {
+
+    for (var i = 0; i < data.items.length; i++) {
+      var li = document.createElement('li');
+
+      var img = document.createElement('img');
+      img.src = data.items[i].avatar_url;
+      li.appendChild(img);
+
+      var a = document.createElement('a');
+      a.href = data.items[i].html_url;
+      a.target = '_blank';
+      a.textContent = data.items[i].login;
+
+      li.appendChild(a);
+
+      listOfUsers.appendChild(li);
+    }
+
+  });
+
 
 });
