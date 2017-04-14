d500637691d99fc002c3b22f70c3b8d0faa6aac4
Eric Sowell <eric.sowell@gmail.com>
2017-04-14 14:57:58 -0500

    ugly user list

---
+++ day-21-github-users/user-list.js
@@ -0,0 +1,42 @@
+var thatOL = document.querySelector('ol');
+var detailsDiv = document.querySelector('#details');
+var template = document.querySelector('#list-template').innerHTML;
+var detailsTemplate = document.querySelector('#detail-template').innerHTML;
+
+var ajaxData;
+
+$.ajax({
+  url: 'https://api.github.com/search/users?q=bananas'
+})
+.done(function(data) {
+
+  ajaxData = data;
+
+  var html = '';
+
+  for (var i = 0; i < data.items.length; i++) {
+
+    var rowClass = 'even';
+    if (i % 2 === 1) {
+      rowClass = 'odd';
+    }
+
+    html += Mustache.render(template, {
+      index: i,
+      login: data.items[i].login,
+      rowClass: rowClass
+    });
+  }
+  thatOL.innerHTML = html;
+});
+
+thatOL.addEventListener('click', function(evt) {
+  var index = Number(evt.target.getAttribute('data-index'));
+  console.log(ajaxData.items[index]);
+
+  var html = Mustache.render(detailsTemplate, ajaxData.items[index]);
+  console.log(html);
+
+  detailsDiv.innerHTML = html;
+
+});
