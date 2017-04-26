eb4e99d9f96c24f8e02af747b58e9ec68528851e
Eric Sowell <eric.sowell@gmail.com>
2017-04-26 09:22:21 -0500

    blank project

---
+++ day-33-react-app/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
