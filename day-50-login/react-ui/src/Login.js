cdc10a3506fa8a79b826e980e8d4cb813cdfc06e
Eric Sowell <eric.sowell@gmail.com>
2017-05-18 18:24:08 -0500

    basic login project

---
+++ day-50-login/react-ui/src/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Login extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Login
+      </div>
+    );
+  }
+
+}
+
+
+module.exports = Login;
