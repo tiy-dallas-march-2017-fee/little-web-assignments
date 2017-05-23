7822f8cbfeadcd1ab6c838c175cfbbbeef9506bd
Eric Sowell <eric.sowell@gmail.com>
2017-05-23 09:10:05 -0500

    Google maps example

--- day-52-refs/src/GoogleMapsExample.js
+++ day-52-refs/src/GoogleMapsExample.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 class GoogleMapsExample extends React.Component {
 
+  componentDidMount() {
+    this.googleMap = new window.google.maps.Map(this.map, {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8
+    });
+
+    var marker = new window.google.maps.Marker({
+      position: { lat: -34.397, lng: 150.644 },
+      map: this.googleMap,
+      title: 'Hover over the marker to see this text!'
+    });
+  }
+
   render() {
     return (
-      <div>Google maps example</div>
+      <div>
+        <h1>Google maps example</h1>
+
+        <div ref={(map) => { this.map = map; }} style={{width: '50%', height: '400px'}}></div>
+      </div>
     );
   }
 
