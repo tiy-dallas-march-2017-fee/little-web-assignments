a325688beea22556df824c312fb1bce76804ca1b
Eric Sowell <eric.sowell@gmail.com>
2017-05-31 15:53:09 -0500

    updated refs example

--- day-52-refs/src/GoogleMapsExample.js
+++ day-52-refs/src/GoogleMapsExample.js
@@ -2,17 +2,39 @@ import React from 'react';
 
 class GoogleMapsExample extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      locations: [
+        { lat: -34.397, lng: 150.644 },
+        { lat: -34.297, lng: 150.842 }
+      ]
+    }
+  }
+
   componentDidMount() {
     this.googleMap = new window.google.maps.Map(this.map, {
       center: { lat: -34.397, lng: 150.644 },
       zoom: 8
     });
 
+    //Put markers in based on state
+    this.state.locations.forEach((loc) => {
+      var marker = new window.google.maps.Marker({
+        position: { lat: loc.lat, lng: loc.lng },
+        map: this.googleMap,
+        title: 'Hover over the marker to see this text!'
+      });
+
+    });
+
+    //Or hard-code the lat/long
     var marker = new window.google.maps.Marker({
       position: { lat: -34.397, lng: 150.644 },
       map: this.googleMap,
       title: 'Hover over the marker to see this text!'
     });
+
   }
 
   render() {
